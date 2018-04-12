6e6823f743cda775e1439de507a312af8fc2d6ac
Oz Ramos <labofoz@gmail.com>
2018-04-12 14:59:42 -0700

    Minimal landing page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template lang="pug">
-#app
-  img(src='./assets/logo.png')
-  router-view
+  .container.grid-xl
+    .columns
+      .column.col-2.col-xl-3.col-md-4.hide-sm
+        Sidebar
+      .column.col-10.col-xl-9.col-md-8
+        router-view
 </template>
 
 <script>
+import Sidebar from '@/components/Sidebar'
+
 export default {
-  name: 'App'
+  components: {
+    Sidebar
+  }
 }
 </script>
 
 <style lang='stylus'>
-  @import '../node_modules/spectre.css/dist/spectre.min.css'
+@import '../node_modules/spectre.css/dist/spectre.min.css'
+
+img
+  max-width: 100%
+  display: inline-block
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+header.navbar
+  section.navbar-section
+    router-link.btn.btn-link(to='/')
+      span.mr-1 Browse<b>Handsfree</b>
+      small {{version}}
+</template>
+
+<script>
+import CONFIG from '@/config.json'
+
+export default {
+  data () {
+    console.log(CONFIG.version)
+    return {
+      version: CONFIG.version
+    }
+  }
+}
+</script>
