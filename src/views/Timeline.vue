ae83a3ee9de493c4effff035935952edb4cff9c6
Oz Ramos <labofoz@gmail.com>
2018-04-19 14:46:50 -0700

    Reverse timeline order

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -14,67 +14,75 @@
           .tile
             .tile-content
               .tile-subtitle(v-html='getMilestoneTitle(item)')
-          iframe(:src='getLink(item)' :onload='resizeFrame(item, this)')
+          iframe(:src='getLink(item)')
 
     br
 </template>
 
 <script>
+import lockr from 'lockr'
+
 export default {
   data () {
+    const isReversed = !lockr.get('view-history-isReversed')
+    let milestones = [
+      {
+        date: '2018-04-17',
+        title: 'Z-Depth',
+        tweet: 'https://twitter.com/LabOfOz/status/986393530414153728'
+      },
+      {
+        date: '2018-04-13',
+        title: 'Asking for Help',
+        tweet: 'https://twitter.com/LabOfOz/status/985043020608290816'
+      },
+      {
+        date: '2018-04-04',
+        title: 'Google Visit',
+        tweet: 'https://twitter.com/LabOfOz/status/981593804313538560'
+      },
+      {
+        date: '2018-03-27',
+        title: 'Clicking',
+        tweet: 'https://twitter.com/LabOfOz/status/978776989090787328'
+      },
+      {
+        date: '2018-03-25',
+        title: 'Eye Tracking',
+        tweet: 'https://twitter.com/LabOfOz/status/978098438989475841'
+      },
+      {
+        date: '2018-03-20',
+        title: 'Commitment',
+        tweet: 'https://twitter.com/LabOfOz/status/976287674016325632'
+      },
+      {
+        date: '2018-03-16',
+        title: 'Tutorial Site',
+        tweet: 'https://twitter.com/LabOfOz/status/974824189235421186'
+      },
+      {
+        date: '2018-03-15',
+        title: '1st Experiment',
+        tweet: 'https://twitter.com/LabOfOz/status/973089354598965248'
+      },
+      {
+        date: '2018-03-04--2',
+        title: '1st Prototype',
+        tweet: 'https://twitter.com/LabOfOz/status/970556829125165056'
+      },
+      {
+        date: '2018-03-04--1',
+        title: 'Ideation',
+        tweet: 'https://twitter.com/LabOfOz/status/970409069717569536'
+      }
+    ]
+
+    isReversed && milestones.reverse()
+
     return {
-      milestones: [
-        {
-          date: '2018-04-17',
-          title: 'Z-Depth',
-          tweet: 'https://twitter.com/LabOfOz/status/986393530414153728'
-        },
-        {
-          date: '2018-04-13',
-          title: 'Asking for Help',
-          tweet: 'https://twitter.com/LabOfOz/status/985043020608290816'
-        },
-        {
-          date: '2018-04-04',
-          title: 'Google Visit',
-          tweet: 'https://twitter.com/LabOfOz/status/981593804313538560'
-        },
-        {
-          date: '2018-03-27',
-          title: 'Clicking',
-          tweet: 'https://twitter.com/LabOfOz/status/978776989090787328'
-        },
-        {
-          date: '2018-03-25',
-          title: 'Eye Tracking',
-          tweet: 'https://twitter.com/LabOfOz/status/978098438989475841'
-        },
-        {
-          date: '2018-03-20',
-          title: 'Commitment',
-          tweet: 'https://twitter.com/LabOfOz/status/976287674016325632'
-        },
-        {
-          date: '2018-03-16',
-          title: 'Tutorial Site',
-          tweet: 'https://twitter.com/LabOfOz/status/974824189235421186'
-        },
-        {
-          date: '2018-03-15',
-          title: '1st Experiment',
-          tweet: 'https://twitter.com/LabOfOz/status/973089354598965248'
-        },
-        {
-          date: '2018-03-04--2',
-          title: '1st Prototype',
-          tweet: 'https://twitter.com/LabOfOz/status/970556829125165056'
-        },
-        {
-          date: '2018-03-04--1',
-          title: 'Ideation',
-          tweet: 'https://twitter.com/LabOfOz/status/970409069717569536'
-        }
-      ]
+      milestones,
+      isReversed
     }
   },
 
@@ -105,15 +113,9 @@ export default {
      * Reverses the order of tweets
      */
     reverseOrder () {
-      console.log('reverse')
-    },
-
-    /**
-     * Resizes the iframe based on it's inner content
-     * @param  {OBJ} milestone The milestone object
-     */
-    resizeFrame (milestone) {
-      console.log('resize', arguments)
+      this.milestones.reverse()
+      this.isReversed = !this.isReversed
+      lockr.set('view-history-isReversed', this.isReversed)
     }
   }
 }
@@ -123,4 +125,6 @@ export default {
   iframe
     border: none
     width: 100%
+    min-height: 760px
+    margin-top: 20px
 </style>
