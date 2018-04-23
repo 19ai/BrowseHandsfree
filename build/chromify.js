cd6023984f6c02269e3f3d685d6242255e736e5a
Oz Ramos <labofoz@gmail.com>
2018-04-22 20:29:15 -0700

    Replaces version strings in compiled chrome extension files

--- build/chromify.js
+++ build/chromify.js
@@ -7,17 +7,30 @@
 
 const ora = require('ora')
 const path = require('path')
-const fs = require('fs-extra')
+const fs = require('fs')
+const fse = require('fs-extra')
 const configs = {
   webpack: require('../config'),
   app: require('../src/config.json')
 }
 const paths = {
-  manifest: 'browser-extensions/chrome/'
+  manifest: 'browser-extensions/chrome/',
+  popup: `${configs.webpack.build.assetsRoot}/chrome-src/popup.html`
 }
 
 const spinner = ora(`Compiling Chrome Extension ${configs.app.version}`)
 spinner.start()
 
-fs.copySync(path.resolve(__dirname, `../${paths.manifest}`), `${configs.webpack.build.assetsRoot}`)
+// Copy over files
+fse.copySync(path.resolve(__dirname, `../${paths.manifest}`), `${configs.webpack.build.assetsRoot}`)
+
+// Update semver
+fs.readFile(paths.popup, 'utf8', (err, data) => {
+  if (err) return console.log(err)
+
+  let result = data.replace(/\$CONFIG\.VERSION/g, configs.app.version)
+
+  fs.writeFileSync(paths.popup, result, 'utf8', (err) => { if (err) return console.log(err) })
+})
+
 spinner.stop()
